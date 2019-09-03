46c3f5440980443fdd8055cee85b94f260821ceb
tmm <blocklocmedia@gmail.com>
2019-09-03 18:08:48 -0400

    added redux features

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as action from 'actions';
 
 class CommentBox extends Component {
 	state = {
@@ -10,6 +12,7 @@ class CommentBox extends Component {
 
 	handleSubmit = (event) => {
 		event.preventDefault();
+		this.props.saveComment(this.state.comment);
 		this.setState({ comment: '' });
 	};
 
@@ -26,4 +29,4 @@ class CommentBox extends Component {
 	}
 }
 
-export default CommentBox;
+export default connect(null, action)(CommentBox);
