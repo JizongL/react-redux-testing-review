7298add0ac34ebef43a560b4a869f8082f6e3f9c
tmm <blocklocmedia@gmail.com>
2019-09-03 12:56:08 -0400

    added first enzyme test for two components existed in App

--- src/components/__tests__/app.test.js
+++ src/components/__tests__/app.test.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+beforeEach(() => {
+	wrapped = shallow(<App />);
+});
 
 it('Shows a comment box', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
-	// Looks inside the div.
-	// console.log(div.innerHTML);
-	expect(div.innerHTML).toContain('Comment Box');
-	// and checks to see if the CommentBox is in there
-
-	ReactDOM.unmountComponentAtNode(div);
+	// return an array of the component, there is one CommentBox component inside App
+	// so the value return should be 1.
+	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+it('Shows a comment list', () => {
+	expect(wrapped.find(CommentList).length).toEqual(1);
 });
