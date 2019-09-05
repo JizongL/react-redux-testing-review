2a53b720e0f6fc118a72b6aa8451b9483edad846
tmm <blocklocmedia@gmail.com>
2019-09-04 22:16:51 -0400

    still debugging with done call back error

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -4,45 +4,40 @@ import Root from 'Root';
 import App from 'components/App';
 import moxios from 'moxios';
 
+let wrapped;
 beforeEach(() => {
 	moxios.install();
 	moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
 		status: 200,
 		response: [ { name: 'Fetch #1' }, { name: 'Fetch #2' } ]
 	});
+	wrapped = mount(
+		<Root>
+			<App />
+		</Root>
+	);
 });
 
 afterEach(() => {
 	moxios.uninstall();
 });
 
-it('can fetch a list of comments and display them', (done) => {
+it('can fetch a list of comments and display them', () => {
 	// Attempt to render the *entire* app
-	const wrapped = mount(
-		<Root>
-			<App />
-		</Root>
-	);
+
 	// find the 'fetchComments' button and click it
 	wrapped.find('.fetch_comments').simulate('click');
 
 	// setTimeout is used because moxio introduces a little delay fetching the data.
 	// so setTimeout makes Jest to have a little delay so it won't throw error.
 	//  Expect to find a list of comments!
-	//
-	setTimeout(() => {
-		wrapped.update();
-		console.log(wrapped.find('li').length);
-		expect(wrapped.find('li').length).toEqual(2);
 
+	moxios.wait(() => {
+		wrapped.update();
+
+		expect(wrapped.find('li').length).toEqual(2);
+		console.log(wrapped.find('li').length);
+		// done();
 		wrapped.unmount();
-		done();
-	}, 3300);
-	// moxios.wait(function() {
-	// 	wrapped.update();
-	// 	console.log(wrapped.find('li').length);
-	// 	expect(wrapped.find('li').length).toEqual(2);
-	// 	done();
-	// 	wrapped.unmount();
-	// });
+	});
 });
